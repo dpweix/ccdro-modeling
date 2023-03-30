dbdaf76b2c0bc7ff5b5be649aae0ceff1f8c3d97
Derek Weix <derekweix@Dereks-MacBook-Air.local>
2023-03-30 14:32:06 -0500

    Changed file name to visualizations

---
+++ scripts/visualizations.R
@@ -0,0 +1,238 @@
+library("tidyverse")
+theme_set(theme_bw())
+theme_update(plot.title = element_text(hjust = 0.5, size = 15),
+             plot.subtitle = element_text(hjust = 0.5, size = 10),
+             strip.placement = "outside",
+             strip.background = element_blank(),
+             strip.text.x = element_text(size = 15))
+library("here")
+library("lubridate")
+library("forecast")
+library("kableExtra")
+
+# Load Data -------------------------------------------------------------------
+df_open <- readRDS(here("data", "open-loop.rds"))
+df_step <- readRDS(here("data", "step-change.rds"))
+
+long_names <- c(sequence = "Sequence",
+                date_time = "Time",
+                hr = "Runtime (hr)",
+                p_f = "Feed Pump Power Consumption (kW)",
+                p_r = "Recirc. Pump Power Consumption (kW)",
+                f_p = "Permeate Flow (L/min)",
+                f_r = "Reject Flow (L/min)",
+                c_p = "Permeate Conductivity (mS/cm)",
+                c_r = "Reject Conductivity (mS/cm)",
+                ph_f = "Feed pH",
+                t_f = "Feed Temperature (C)")
+
+# Open Loop Time Series Plots ---------------------------------------------
+df_open |> 
+  pivot_longer(c(p_f, p_r, f_p, f_r, c_p, c_r, ph_f, t_f)) |> 
+  ggplot(aes(hr, value, color = sequence)) + 
+  geom_line(alpha = 1) +
+  facet_wrap(vars(name), labeller = as_labeller(long_names),
+             ncol = 2, scales = "free_y") +
+  labs(x = "Runtime (Hours)", y = "", color = "Sequence") +
+  scale_color_viridis_d()
+
+ggsave(here("figures", "open-loop-sequences.png"),
+       width = 25, height = 40, units = 'cm')
+
+df_step |> 
+  filter(sequence == 10) |> 
+  pivot_longer(c(p_f, p_r, f_p, f_r, c_p, c_r, t_f)) |> 
+  ggplot(aes(hr, value)) + 
+  geom_point(shape = 21) +
+  facet_wrap(vars(name), labeller = as_labeller(long_names),
+             ncol = 2, scales = "free_y") +
+  labs(x = "Runtime (Hours)", y = "", color = "Sequence") +
+  scale_color_viridis_d()
+
+ggsave(here("figures", "step-change-sequences.png"),
+       width = 25, height = 40, units = 'cm')
+
+# Predict Next Sequence from Previous Sequence --------------------------------
+
+start <- 300 # forecast begins at observation number 100
+
+# ARIMAX Models - FPPC
+fit_arx_pf <- 1:10 |> 
+  map(\(k) {
+    
+    # Matrix of exogenous variables
+    xreg <- dat[[k]] |> select(c("f_p", "c_r")) |> as.matrix()
+    
+    # Fitted model
+    auto.arima(dat[[k]]$p_f, xreg = xreg)
+  })
+
+# Semi-Empirical Models - FPPC
+fit_sem_pf <- 1:10 |> 
+  map(\(k) {
+    
+    # Fit semi-empirical model
+    lm(p_f ~ f_p + c_r, dat[[k]])
+  })
+
+# Predictions ARIMAX - FPPC
+pred_arx_pf <- 2:10 |> 
+  map(\(k) {
+    xreg_start <- fit_arx_pf[[k]]$xreg[1:start, ]
+    xreg_forecast <- fit_arx_pf[[k]]$xreg[-c(1:start), ]
+    
+    # Fit with model estimated on previous experiment.
+    Arima(y = dat[[k]]$p_f[1:start], model = fit_arx_pf[[k-1]],
+          xreg = xreg_start) |> 
+      forecast(xreg = xreg_forecast)
+  })
+
+# Predictions Semi-Empirical - FPPC
+pred_sem_pf <- 2:10 |> 
+  map(\(k) {
+    predict(fit_sem_pf[[k-1]], dat[[k]])
+  })
+
+
+# Data frame of Observed and Predicted - FPPC
+pred_df_pf <- 2:10 |> 
+  map_dfr(\(k) {
+    bind_rows(tibble(experiment = k, type = "Observed",
+                     value = dat[[k]]$p_f[-c(1:start)],
+                     date_time = dat[[k]]$date_time[-c(1:start)]),
+              tibble(experiment = k, type = "Arimax",
+                     value = pred_arx_pf[[k-1]]$mean,
+                     date_time = dat[[k]]$date_time[-c(1:start)]),
+              tibble(experiment = k, type = "Semi-Empirical",
+                     value = pred_sem_pf[[k-1]][-c(1:start)],
+                     date_time = dat[[k]]$date_time[-c(1:start)]))
+  }) |> 
+  mutate(experiment = as_factor(experiment),
+         type = as_factor(type))
+
+# Plot of observed and predicted - FPPC
+pred_df_pf |> 
+  ggplot(aes(date_time, value, color = type)) +
+  geom_line() + 
+  facet_wrap(~ experiment, scales = "free_x") +
+  labs(x = "", y = "", color = "",
+       title = "Open Loop Experiments - Feed Pump Power Consumption")
+
+ggsave("~/git/reports/CCDRO-modeling/figures/open-loop-fppc.png",
+       width = 25, height = 15, units = 'cm')
+
+
+
+# Predict Next Sequence from Previous Sequence --------------------------------
+
+start <- 300 # forecast begins at observation number 100
+
+# ARIMAX Models - RPPC
+fit_arx_pr <- 1:10 |> 
+  map(\(k) {
+    
+    # Matrix of exogenous variables
+    xreg <- dat[[k]] |> select(c("f_r", "c_r")) |> as.matrix()
+    
+    # Fitted model
+    auto.arima(dat[[k]]$p_r, xreg = xreg)
+  })
+
+# Semi-Empirical Models - RPPC
+fit_sem_pr <- 1:10 |> 
+  map(\(k) {
+    
+    # Fit semi-empirical model
+    lm(p_r ~ f_r + I(f_r^2) + 0, dat[[k]])
+  })
+
+# Predictions ARIMAX - RPPC
+pred_arx_pr <- 2:10 |> 
+  map(\(k) {
+    xreg_start <- fit_arx_pr[[k]]$xreg[1:start, ]
+    xreg_forecast <- fit_arx_pr[[k]]$xreg[-c(1:start), ]
+    
+    # Fit with model estimated on previous experiment.
+    Arima(y = dat[[k]]$p_r[1:start], model = fit_arx_pr[[k-1]],
+          xreg = xreg_start) |> 
+      forecast(xreg = xreg_forecast)
+  })
+
+# Predictions Semi-Empirical - RPPC
+pred_sem_pr <- 2:10 |> 
+  map(\(k) {
+    predict(fit_sem_pr[[k-1]], dat[[k]])
+  })
+
+
+# Data frame of Observed and Predicted - RPPC
+pred_df_pr <- 2:10 |> 
+  map_dfr(\(k) {
+    bind_rows(tibble(experiment = k, type = "Observed",
+                     value = dat[[k]]$p_r[-c(1:start)],
+                     date_time = dat[[k]]$date_time[-c(1:start)]),
+              tibble(experiment = k, type = "Arimax",
+                     value = pred_arx_pr[[k-1]]$mean,
+                     date_time = dat[[k]]$date_time[-c(1:start)]),
+              tibble(experiment = k, type = "Semi-Emperical",
+                     value = pred_sem_pr[[k-1]][-c(1:start)],
+                     date_time = dat[[k]]$date_time[-c(1:start)]))
+  }) |> 
+  mutate(experiment = as_factor(experiment),
+         type = as_factor(type))
+
+# Plot of observed and predicted - RPPC
+pred_df_pr |> 
+  ggplot(aes(date_time, value, color = type)) +
+  geom_line() + 
+  facet_wrap(~ experiment, scales = "free_x") +
+  labs(x = "", y = "", color = "",
+       title = "Open Loop Experiments - Recirculation Pump Power Consumption")
+
+ggsave("~/git/reports/CCDRO-modeling/figures/open-loop-rppc.png",
+       width = 25, height = 15, units = 'cm')
+
+
+### MAE ---------------------------------------------------------------------
+# Residuals by Model - RPPC
+mae_rppc <- pred_df_pr |> 
+  pivot_wider(names_from = type,
+              values_from = value,
+              values_fn = "list") |> 
+  unnest(cols = c("Observed", "Arimax", "Semi-Emperical")) |> 
+  pivot_longer(c("Arimax", "Semi-Emperical")) |> 
+  mutate(name = as_factor(name)) |> 
+  group_by(name, experiment) |> 
+  summarise(mae = mean(abs(Observed-value))) |> 
+  ungroup() |> 
+  pivot_wider(names_from = name,
+              names_prefix = "",
+              values_from = mae)  |> 
+  mutate(across(where(is.numeric), round, 3))
+
+
+# Residuals by Model - FPPC
+mae_fppc <- pred_df_pf |> 
+  pivot_wider(names_from = type,
+              values_from = value,
+              values_fn = "list") |> 
+  unnest(cols = c("Observed", "Arimax", "Semi-Empirical")) |> 
+  pivot_longer(c("Arimax", "Semi-Empirical")) |> 
+  mutate(name = as_factor(name)) |> 
+  group_by(name, experiment) |> 
+  summarise(mae = mean(abs(Observed-value))) |> 
+  ungroup() |> 
+  pivot_wider(names_from = name,
+              names_prefix = "",
+              values_from = mae) |> 
+  mutate(across(where(is.numeric), round, 3)) #|> 
+# kbl(format = "latex", booktabs = TRUE)
+
+# LaTeX Table: Linear Data
+
+bind_cols(mae_fppc, mae_rppc[, -1], .name_repair = "minimal") |> 
+  kbl(format = "latex", booktabs = TRUE, linesep =  c('', '', '', '\\addlinespace'),
+      escape = FALSE) |> 
+  add_header_above(c(" " = 1,
+                     "Feed Pump Power Consumption" = 2,
+                     "Recirculation Pump Power Consumption" = 2))
